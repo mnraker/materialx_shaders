146b92a2226c4c1f416f7b49a15e9bce2aaade1b
Alex Fuller <boberfly@gmail.com>
2026-08-10 19:28:29 +1000

    macOS build

--- cmake/MaterialxShadersLinkOptions.cmake
+++ cmake/MaterialxShadersLinkOptions.cmake
@@ -2,10 +2,8 @@
 # Copyright Contributors to the Moonray Project
 
 function(${PROJECT_NAME}_link_options target)
-    if(NOT WIN32)
-        target_link_options(${target}
-            PRIVATE
-                -Wl,--enable-new-dtags
-        )
-    endif()
+    target_link_options(${target}
+        PRIVATE
+            ${GLOBAL_LINK_FLAGS}
+    )
 endfunction()
